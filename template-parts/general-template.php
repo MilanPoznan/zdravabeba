84583474ddf75b2ee269ada2939a22f1d4f98647
Djordje Stojanovic <djordje@jl.is>
2019-02-10 11:05:12 +0100

    Adding new bem structure

--- template-parts/general-template.php
+++ template-parts/general-template.php
@@ -13,20 +13,21 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post(); ?>
-
-		<h2 class="general-title"><?php echo get_field('text_title'); ?></h2>
-		<div class="general-content">
+		<div class="general-text wrapper">
+		<h2 class="general-text__title"><?php echo get_field('text_title'); ?></h2>
+		<div class="general-text__content">
       <?php echo get_field('text_content'); ?>
 
-      <div class="general-content__social-share">Udostępnij:
+      <div class="general-text__social-share">Udostępnij:
 				<!-- To Do-->
 			</div>
 
-			<div class="general-content__purple-box">
-				<h3 class="general-content__purple-box-title"><?php echo get_field('box_title'); ?></h3>
-				<p class="general-content__purple-box-text"><?php echo get_field('box_content'); ?></p>
+			<div class="general-text__purple-box">
+				<h3 class="general-text__purple-box-title"><?php echo get_field('box_title'); ?></h3>
+				<p class="general-text__purple-box-text"><?php echo get_field('box_content'); ?></p>
 			</div>
     </div>
+		</div>
 		<?php
 		endwhile; // End of the loop.
 		?>
