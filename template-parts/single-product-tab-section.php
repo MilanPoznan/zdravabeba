be15542de22616b7cc85da7d5928c45e7367fba6
Milan Poznan <poznan.milan@gmail.com>
2019-02-14 10:54:10 +0100

    in progress

--- template-parts/single-product-tab-section.php
+++ template-parts/single-product-tab-section.php
@@ -28,12 +28,55 @@
         <?php the_sub_field('table_description'); ?>
       </div>
     <?php } ?>
-    <div class="single-tab__desktop">
 
-
-    </div>
   <?php endwhile; ?>
   </div>
+  <div class="single-tab__desktop">
+    <div class="single-tab__wrapper">
+      <h2>Saznaj Vise</h2>
+      <div class="single-tab__header">
+        <?php while ( have_rows('repeater_tab') ) : the_row(); ?>
+          <div class="single-tab__header-column">
+              <?php the_sub_field('title'); ?>
+          </div>
+        <?php endwhile; ?>
+
+
+      </div>
+      <div class="single-tab__content">
+
+      <?php while ( have_rows('repeater_tab') ) : the_row(); ?>
+        <div class="single-tab__content-field">
+          <?php if (get_sub_field('has_table')) {  ?>
+            <?php if (have_rows('table_repeater')) {  ?>
+              <!-- TABLE INFORMTION -->
+            <div class="single-tab__table">
+              <?php while( have_rows('table_repeater') ): the_row(); ?>
+                <div class="single-tab__table-row" value="<?php echo get_row_index(); ?>">
+                  <div class="single-tab__table-title">
+                    <?php the_sub_field('table_title'); ?>
+                  </div>
+                  <div class="single-tab__table-desc">
+                    <?php the_sub_field('table_desc'); ?>
+                  </div>
+                </div>
+              <?php endwhile; ?>
+            </div>
+            <!-- END OF TABLE -->
+          <?php } ?>
+          <?php } else { ?>
+            <!-- DESCRIPTION  -->
+            <div class="single-tab__description" value="<?php echo get_row_index(); ?>">
+              <?php the_sub_field('table_description'); ?>
+            </div>
+          <?php } ?>
+        </div>
+      <?php endwhile; ?>
+    </div>
+
+    </div>
+  </div>
 <?php endif; ?>
+
 
 </section>
