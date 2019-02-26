722e5cd43c32aad13eb5be1747527a3ac70d8c50
Djordje Stojanovic <djordje@jl.is>
2019-02-26 12:32:51 +0100

    Adding and linking instagram icon in the footer 26.feb

--- footer.php
+++ footer.php
@@ -53,6 +53,7 @@
 					<p class="footer__social--title">Pratite nas na:</p>
 					<div class="footer__social-icons">
 						<a href="https://www.facebook.com/FemibionRS/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+						<a href="https://www.instagram.com/femibion.srbija/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>						
 					  <!-- <a href="#" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
 						<a href="#" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
 						<a href="https://www.youtube.com" target="_blank"><i class="fa fa-youtube-play"></i></a> -->
